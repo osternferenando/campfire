ae82d786eb442e5bebd4bccd417d00fd5eaa4fe2
osternferenando <osternferenando01@gmail.com>
2026-07-21 01:20:15 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+FIREBASE_CONFIG: {
+  apiKey: "AIzaSyAB_fZa8AWajzmQwZGkE7EDkwcx67x_Ojo",
+  authDomain: "campfire-app-14ea3.firebaseapp.com",
+  projectId: "campfire-app-14ea3",
+  storageBucket: "campfire-app-14ea3.firebasestorage.app",
+  messagingSenderId: "95995169528",
+  appId: "1:95995169528:web:f7222b9dba46ff31e2490d"
+}
